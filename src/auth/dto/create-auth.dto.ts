294fe56cbd6a2a358ee6762046bfc911c144dc0b
bezo <bezo16@protonmail.ch>
2025-07-11 09:54:16 +0200

    feat: added better input validation

--- src/auth/dto/create-auth.dto.ts
+++ src/auth/dto/create-auth.dto.ts
@@ -1,12 +1,23 @@
-import { IsString } from 'class-validator';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 export class CreateAuthDto {
   @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
+  @MaxLength(50)
   username: string;
 
   @IsString()
+  @IsEmail()
   email: string;
 
   @IsString()
+  @MinLength(8)
   password: string;
 }
