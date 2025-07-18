8f85167be72d926316345aa32620aa6127e9315b
bezo <bezo16@protonmail.ch>
2025-07-18 11:15:27 +0200

    feat: added swagger

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,33 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+const config = new DocumentBuilder()
+  .setTitle('Social Platform API')
+  .setDescription('API documentation for the Social Platform')
+  .setVersion('1.0')
+  .addTag('beta')
+  .addBearerAuth()
+  .build();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // middleware
+  app.use(cookieParser());
   app.enableCors({
-    origin: 'http://localhost:3000', // Replace with your frontend's URL
+    origin: process.env.FRONTEND_URL,
     credentials: true,
   });
-  app.use(cookieParser());
+
+  // pipes
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT ?? 3000);
+
+  // swagger
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
+  await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
