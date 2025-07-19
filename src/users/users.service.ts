eca6804fbbe8bd42a1d870a12d0bbb68ace7959b
bezo <bezo16@protonmail.ch>
2025-07-19 19:08:43 +0200

    feat: random users endpoint

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { FindOneUserDto } from './dto/find-one-user.dto';
 import db from 'lib/drizzle';
-import { eq } from 'drizzle-orm';
+import { eq, sql } from 'drizzle-orm';
 import { usersTable } from 'lib/drizzle/schema';
 
 type CreateUserParas = {
@@ -13,39 +13,37 @@ type CreateUserParas = {
 @Injectable()
 export class UsersService {
   async findOneUserByUsername(userInfo: FindOneUserDto) {
-    try {
-      const user = await db
-        .select()
-        .from(usersTable)
-        .where(eq(usersTable.username, userInfo.username))
-        .limit(1);
+    const user = await db
+      .select()
+      .from(usersTable)
+      .where(eq(usersTable.username, userInfo.username))
+      .limit(1);
 
-      if (user.length === 0) {
-        throw new UnauthorizedException('Failed to login');
-      }
+    if (user.length === 0) throw new UnauthorizedException('Failed to login');
 
-      return user[0];
-    } catch (error) {
-      console.error(error);
-      throw new UnauthorizedException('Failed to login');
-    }
+    return user[0];
   }
 
   async createUser({ username, email, password_hash }: CreateUserParas) {
-    try {
-      const [createdUser] = await db
-        .insert(usersTable)
-        .values({
-          username,
-          email,
-          password_hash,
-        })
-        .returning();
+    const [createdUser] = await db
+      .insert(usersTable)
+      .values({
+        username,
+        email,
+        password_hash,
+      })
+      .returning();
 
-      return createdUser;
-    } catch (error) {
-      console.error(error);
-      throw new UnauthorizedException('Failed to create user');
-    }
+    return createdUser;
+  }
+
+  async findRandomUsers() {
+    const users = await db
+      .select()
+      .from(usersTable)
+      .orderBy(sql`RANDOM()`)
+      .limit(10);
+
+    return users;
   }
 }
