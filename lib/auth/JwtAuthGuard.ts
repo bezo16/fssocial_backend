5263186c445326c9f49877ef1be255b613b2db68
bezo <bezo16@protonmail.ch>
2025-07-25 13:46:45 +0200

    refactor: JwtAuthGuard now takes header for auth

--- lib/auth/JwtAuthGuard.ts
+++ lib/auth/JwtAuthGuard.ts
@@ -5,20 +5,22 @@ import {
   UnauthorizedException,
 } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
+import { User } from 'lib/drizzle/schema';
 
 @Injectable()
 export class JwtAuthGuard implements CanActivate {
   constructor(private jwtService: JwtService) {}
 
   canActivate(context: ExecutionContext): boolean {
-    const request = context.switchToHttp().getRequest<{
-      cookies?: { authToken?: string };
-      user?: Record<string, any>;
-    }>();
-    const token = request.cookies?.authToken;
+    const request = context
+      .switchToHttp()
+      .getRequest<Request & { user: User }>();
+    const token = request.headers['authorization'] as string;
+
     if (!token) throw new UnauthorizedException('No token provided');
     try {
-      const user: Record<string, any> = this.jwtService.verify(token, {
+      const user: User = this.jwtService.verify(token, {
         secret: process.env.JWT_SECRET as string,
       });
       request.user = user;
